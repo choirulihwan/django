9e881aa5cd7a8fd6e1e292b4307ab2489265e6fa
choirulihwan <aristhu_oracle@yahoo.com>
2020-02-03 06:42:16 +0700

    pagination

--- realestate/listings/views.py
+++ realestate/listings/views.py
@@ -1,11 +1,17 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from listings.models import Listing
 
 # Create your views here.
 def index(request):
-    listings = Listing.objects.all()
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+
+    paginator = Paginator(listings, 3)
+    page = request.GET.get('page')
+    paged_listing = paginator.get_page(page)
+
     context = {
-        'listings':listings
+        'listings': paged_listing
     }
 
     return render(request, 'listings/listings.html', context)
