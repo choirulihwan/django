aa138f5d763f39cca17b594d5a0f463a3f9f9162
choirulihwan <aristhu_oracle@yahoo.com>
2020-06-03 15:17:00 +0700

    bootstrap container and container fluid done

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -18,6 +18,7 @@ from django.conf.urls.static import static
 
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 # from products.views import \
 #     ProductListView, product_list_view,\
@@ -34,6 +35,7 @@ urlpatterns = [
     path('login/', login_page, name="login"),
     path('register/', register_page, name="register"),
     path('products/', include('products.urls', namespace='products')),
+    path('bootstrap/', TemplateView.as_view(template_name='bootstrap/example.html')),
 
     # path('featured/', ProductFeaturedListView.as_view()),
     # path('featured/<int:pk>', ProductFeaturedDetailView.as_view()),
