440c74efe8473abcf7003c30ad039a235fd16e38
choirul <aristhu_oracle@yahoo.com>
2020-06-14 13:39:58 -0700

    cart views

--- ecommerce/carts/views.py
+++ ecommerce/carts/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+from carts.models import Cart
+
+
 def cart_home(request):
     # print(dir(request.session))
     # request.session.set_expiry(300)
     # key = request.session.session_key
     # print(key)
-
+    del request.session['cart_id']
     cart_id = request.session.get('cart_id', None)
 
     if cart_id is None:
-        print('create new cart')
-        request.session['cart_id'] = 12
+        # print('create new cart')
+        cart_obj = Cart.objects.create(user=None)
+        request.session['cart_id'] = cart_obj.id
     else:
         print('Cart id exists')
+        print(cart_id)
+        cart_obj = Cart.objects.get(id=cart_id)
     return render(request, "carts/home.html", {})
