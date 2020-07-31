9bb23dbd4e7a0e653296ae1068b458855a4fac73
choirul84 <aristhu_oracle@yahoo.com>
2020-07-31 13:57:22 +0700

    add requirements.txt

--- notes/frontend/src/App.js
+++ notes/frontend/src/App.js
@@ -92,27 +92,6 @@ class App extends Component {
 }
 
 
-/*function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello world</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button color="danger">Hello</Button>
-      </header>
-    </div>
-  );
-}*/
+
 
 export default App;
