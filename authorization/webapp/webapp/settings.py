d9fe896b0b70e5d144173d992d4400e985e57442
choirul84 <aristhu_oracle@yahoo.com>
2020-11-01 12:29:06 +0700

    hide secret key

--- authorization/webapp/webapp/settings.py
+++ authorization/webapp/webapp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'h^d=e3yilh&lm)0u1)&612i2nxkqcvs89%0w)lg9rkd0y85rmd'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
